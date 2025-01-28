f79ffbddb0870721e9fff81698e3dc2b438bd166
PythonHackerr <mishamarfenko@gmail.com>
2025-01-27 19:01:45 -0500

    Updated Android Dependency

--- Assets/Radar/Resources/Editor/RadarSettingsEditorWindow.cs
+++ Assets/Radar/Resources/Editor/RadarSettingsEditorWindow.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR
 using UnityEditor;
 using UnityEngine;
 using RadarSDK;
@@ -202,3 +203,4 @@ public class RadarSettingsEditorWindow : EditorWindow
         Debug.Log("Radar SDK settings saved.");
     }
 }
+# endif
