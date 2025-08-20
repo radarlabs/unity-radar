15d7c65e2b439670f99bfe648105b51bcf284bca
Cameron Morrow <cameron.morrow@toptal.com>
2025-08-20 14:03:36 -0400

    renamed VerifiedLocationData

--- Assets/Radar/Scripts/Models/RadarVerifiedLocationToken.cs
+++ Assets/Radar/Scripts/Models/RadarVerifiedLocationToken.cs
@@ -1,6 +1,3 @@
-using UnityEngine;
-using System.Collections.Generic;
-
 namespace RadarSDK
 {
     /// <summary>
@@ -8,35 +5,18 @@ namespace RadarSDK
     /// Check out the <a href="https://radar.com/documentation/fraud">fraud documentation</a> for more information.
     /// </summary>
     [System.Serializable]
-    public struct RadarVerifiedLocationToken
+    public class RadarVerifiedLocationToken
     {
-        public Location? Location { get; set; }
-        /// <summary>
-        /// The user.
-        /// </summary>
-        public User user;
+        public bool Passed { get; set; }
+        public string Token { get; set; }
+        public long ExpiresAt { get; set; }
+        public long ExpiresIn { get; set; }
 
-        /// <summary>
-        /// A boolean indicating whether the user passed all jurisdiction and fraud detection checks.
-        /// </summary>
-        public bool passed;
 
-        // The commented out fields are not currently in use.
-        // Prefer to leave them commented out due to case sensitivity.
+        public override string ToString()
+        {
+            return $"Token: {Token}, Passed: {Passed}, ExpiresAt: {ExpiresAt}, ExpiresIn: {ExpiresIn}";
+        }
 
-        /// <summary>
-        /// An array of events.
-        /// </summary>
-        public List<Event> events { get; set; }
-
-        /// <summary>
-        /// A signed JSON Web Token (JWT) containing the user and array of events. Verify the token server-side using your secret key.
-        /// </summary>
-        public string token { get; set; }
-
-        /// <summary>
-        /// The number of seconds until the token expires.
-        /// </summary>
-        public int expiresIn { get; set; }
     }
 }
