d4a1b9f347d831c9509f1cdf9e1f9e15066aa106
cameron <cameron.morrow@radar.com>
2025-11-03 08:24:51 -0500

    fixing release pipeline

---
+++ Assets/Editor/ExportPackage.cs
@@ -0,0 +1,21 @@
+using UnityEditor;
+using UnityEngine;
+
+public class ExportPackage
+{
+    [MenuItem("Tools/Export Package")]
+    public static void Run()
+    {
+        string packagePath = "RadarUnitySDK.unitypackage";
+        string[] assetPaths = new string[]
+        {
+            "Assets/ExternalDependencyManager",   // Include relevant paths
+            "Assets/Plugins",
+            "Assets/Radar",
+            "Assets/Settings",
+        };
+
+        AssetDatabase.ExportPackage(assetPaths, packagePath, ExportPackageOptions.Recurse);
+        Debug.Log("Unity package exported successfully to: " + packagePath);
+    }
+}
